61b084f492761f7d6487f742ffa8c13017374899
Danilo Caporale <danilo.caporale@gmail.com>
2020-04-13 17:34:34 +0200

    Added test bluetooth module

---
+++ web/js/root-ble/main.js
@@ -0,0 +1,36 @@
+
+window.LOG = 1;
+
+var bleDevice = new BleDevice(Root.identifier, Root.services);
+
+bleDevice.addEventListener('connected', function (event) {
+  btnDisconnect.removeAttribute('disabled');
+  btnScanAndConnect.setAttribute('disabled', 'disabled');
+
+  var root = new Root(bleDevice);
+  root.setup(rootIsSetup);
+});
+
+bleDevice.addEventListener('disconnected', function (event) {
+  btnScanAndConnect.removeAttribute('disabled');
+  btnDisconnect.setAttribute('disabled', 'disabled');
+});
+
+var btnScanAndConnect = document.getElementById('btnScanAndConnect')
+var btnDisconnect = document.getElementById('btnDisconnect')
+
+btnScanAndConnect.addEventListener('pointerup', scanAndConnect)
+btnDisconnect.addEventListener('pointerup', disconnect)
+
+function scanAndConnect (event) {
+  bleDevice.scanAndConnect();
+}
+
+function disconnect (event) {
+  bleDevice.disconnect();
+}
+
+function rootIsSetup (root) {
+  console.log('start using root', root);
+  window.root = root;
+}
