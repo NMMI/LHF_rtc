9d927a07d240da92b71a7fb48d62232181ada228
Danilo Caporale <danilo.caporale@gmail.com>
2020-04-13 17:21:21 +0200

    Updated

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 # git clone https://gitlab.com/danresearch/toolbox/webrtc_p2p_call
 
 cd web
+git clone https://github.com/matths/root-robot-ble-javascript.git root-robot
 npm install -g express-generator # possibly not needed
 npm install express
 npm install websocket
